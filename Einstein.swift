9af1bb790404e642e583e547cead362fb5d05da2
Jessah Tumbaga <jessah.tumbaga@stu.ocsb.ca>
2024-02-29 15:35:03 +0000

    Addedv2 Einstein.swift

--- Einstein.swift
+++ Einstein.swift
@@ -1,12 +1,12 @@
 //
-//  MathOperators.swift
+//  Einstein.swift
 //
 //  Created by Jessah
 //  Created on 2024-02-28
 //  Version 1.0
 //  Copyright (c) 2024 Jessah. All rights reserved.
 //
-//  Using math operators in swift.
+//  Uses Einstein's formula to calculate mass.'
 
 // Import for power.
 import Foundation
@@ -18,28 +18,25 @@ enum InputError: Error {
   // invalid input error
   case invalidInput
 }
-func userInput() throws -> Double {
-  // 
+
+do {
+  // Get user input
   print("Enter a mass: ")
   guard let input = readLine(), let userMass = Double(input) else {
     throw InputError.invalidInput
   }
-  return userMass
-}
 
-do {
-  // calls function to get user input and 'tries' it
-  let userMass = try userInput()
-
+  // User input above 0
   if (userMass > 0){
     let energy = userMass * pow(Double(speedOfLight), 2)
     print("The the energy of the mass is \(round(energy * 1000)/1000)")
 
   } else {
+    // User input below 1
     print("Energy cannot be negative")
   }
 
-  // if user imputed invalid input
+  // if user enters a string
 } catch InputError.invalidInput {
   print("Invalid input, please enter a number.")
 }
